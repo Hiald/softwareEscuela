31d4141ac549337f411c18a850337c7f3b36a2f9
Hiald <jonathan.seminario11@gmail.com>
2020-10-05 13:38:59 -0500

    crud para clase - jonathan

---
+++ frontend_SoftColegio/frontend_SoftColegio/Controllers/ClaseController.cs
@@ -0,0 +1,216 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using frontendED;
+using frontendUtil;
+
+namespace frontend_SoftColegio.Controllers
+{
+    public class ClaseController : Controller
+    {
+        //CRUD para administrar
+        public ActionResult ClaseGestion()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<JsonResult> InsertarClaseGestion(int idgrado, string nombre, string descripcion
+            , string rutaenlace, string rutavideo, int categoria, string imagen, string imagenruta
+            , int orden)
+        {
+            try
+            {
+                var objResultado = new object();
+                string wfechaRegistro = DateTime.Now.ToString();
+                int idGenerado = -1;
+                Int16 estado = 1;
+                using (var client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri(MvcApplication.wsRouteSchoolBackend);
+                    client.DefaultRequestHeaders.Clear();
+                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                    HttpResponseMessage ResRegistrarCuenta = await client.GetAsync("api/clase/wsInsertarClase?widgrado=" + idgrado
+                        + "&wnombre=" + nombre + "&wdescripcion=" + descripcion + "&wrutaenlace=" + rutaenlace
+                        + "&wrutavideo=" + rutavideo + "&wcategoria=" + categoria + "&wimagen=" + imagen
+                        + "&wimagenruta=" + imagenruta + "&worden=" + orden + "&westado=" + estado
+                        + "&wfecharegistro=" + wfechaRegistro);
+
+                    if (ResRegistrarCuenta.IsSuccessStatusCode)
+                    {
+                        var rwsapi = ResRegistrarCuenta.Content.ReadAsAsync<string>().Result;
+                        idGenerado = int.Parse(rwsapi);
+
+                        if (idGenerado == -1 || idGenerado == 0)
+                        {
+                            //error
+                            objResultado = new
+                            {
+                                iResultado = -1,
+                                iResultadoIns = "Ha ocurrido un error, intentalo nuevamente. Error: BCK"
+                            };
+                            return Json(objResultado);
+                        }
+                    }
+                }
+
+                objResultado = new
+                {
+                    iResultado = 1,
+                    iResultadoIns = "Registrado correctamente"
+                };
+                return Json(objResultado);
+            }
+            catch (Exception ex)
+            {
+                //UtlLog.toWrite(UtlConstantes.PizarraWEB, UtlConstantes.LogNamespace_PizarraWEB, this.GetType().Name.ToString(), MethodBase.GetCurrentMethod().Name, UtlConstantes.LogTipoError, "", ex.StackTrace.ToString(), ex.Message.ToString());
+                return Json(ex);
+            }
+
+        }
+
+        [HttpPost]
+        public async Task<JsonResult> ActualizarClaseGestion(int widclase, int wsidgrado, string wsnombre, string wsdescripcion
+                , string wsrutaenlace, string wsrutavideo, int wscategoria, string wsimagen, string wsimagenruta
+                , int wsorden)
+        {
+            try
+            {
+                var objResultado = new object();
+                string wfechaRegistro = DateTime.Now.ToString();
+                int idGenerado = -1;
+                Int16 wsestado = 1;
+                using (var client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri(MvcApplication.wsRouteSchoolBackend);
+                    client.DefaultRequestHeaders.Clear();
+                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                    HttpResponseMessage ResRegistrarCuenta = await client.GetAsync("api/clase/wsActualizarClase?widclase=" + widclase 
+                        + "&wsidgrado=" + wsidgrado + "&wsnombre=" + wsnombre + "&wsdescripcion=" + wsdescripcion
+                        + "&wsrutaenlace=" + wsrutaenlace + "&wsrutavideo="+ wsrutavideo + "&wscategoria=" + wscategoria 
+                        + "&wsimagen=" + wsimagen + "&wsimagenruta=" + wsimagenruta
+                        + "&wsorden=" + wsorden + "&wsestado="+ wsestado + "&fecharegistro="+ wfechaRegistro);
+
+                    if (ResRegistrarCuenta.IsSuccessStatusCode)
+                    {
+                        var rwsapi = ResRegistrarCuenta.Content.ReadAsAsync<string>().Result;
+                        idGenerado = int.Parse(rwsapi);
+
+                        if (idGenerado == -1 || idGenerado == 0)
+                        {
+                            //error
+                            objResultado = new
+                            {
+                                iResultado = -1,
+                                iResultadoIns = "Ha ocurrido un error, intentalo nuevamente. Error: BCK"
+                            };
+                            return Json(objResultado);
+                        }
+                    }
+                }
+
+                objResultado = new
+                {
+                    iResultado = 1,
+                    iResultadoIns = "Registrado correctamente"
+                };
+                return Json(objResultado);
+            }
+            catch (Exception ex)
+            {
+                //UtlLog.toWrite(UtlConstantes.PizarraWEB, UtlConstantes.LogNamespace_PizarraWEB, this.GetType().Name.ToString(), MethodBase.GetCurrentMethod().Name, UtlConstantes.LogTipoError, "", ex.StackTrace.ToString(), ex.Message.ToString());
+                return Json(ex);
+            }
+
+        }
+
+        [HttpPost]
+        public async Task<JsonResult> EliminarClaseGestion(int widclase)
+        {
+            try
+            {
+                var objResultado = new object();
+                int idGenerado = -1;
+                using (var client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri(MvcApplication.wsRouteSchoolBackend);
+                    client.DefaultRequestHeaders.Clear();
+                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                    HttpResponseMessage ResRegistrarCuenta = await client.GetAsync("api/clase/wsEliminarClase?wsidclase=" + widclase);
+
+                    if (ResRegistrarCuenta.IsSuccessStatusCode)
+                    {
+                        var rwsapi = ResRegistrarCuenta.Content.ReadAsAsync<string>().Result;
+                        idGenerado = int.Parse(rwsapi);
+
+                        if (idGenerado == -1 || idGenerado == 0)
+                        {
+                            //error
+                            objResultado = new
+                            {
+                                iResultado = -1,
+                                iResultadoIns = "Ha ocurrido un error, intentalo nuevamente. Error: BCK"
+                            };
+                            return Json(objResultado);
+                        }
+                    }
+                }
+
+                objResultado = new
+                {
+                    iResultado = 1,
+                    iResultadoIns = "Registrado correctamente"
+                };
+                return Json(objResultado);
+            }
+            catch (Exception ex)
+            {
+                //UtlLog.toWrite(UtlConstantes.PizarraWEB, UtlConstantes.LogNamespace_PizarraWEB, this.GetType().Name.ToString(), MethodBase.GetCurrentMethod().Name, UtlConstantes.LogTipoError, "", ex.StackTrace.ToString(), ex.Message.ToString());
+                return Json(ex);
+            }
+
+        }
+
+        [HttpPost]
+        public async Task<JsonResult> ListarClase(int idcurso)
+        {
+            try
+            {
+                var objResultado = new object();
+
+                edClase loenCategoria = new edClase();
+                using (var client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri(MvcApplication.wsRouteSchoolBackend);
+                    client.DefaultRequestHeaders.Clear();
+                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                    HttpResponseMessage Reslistarusu = await client.GetAsync("api/clase/wsListarClaseCurso?widcurso=" + idcurso);
+                    if (Reslistarusu.IsSuccessStatusCode)
+                    {
+                        var rwsapilu = Reslistarusu.Content.ReadAsAsync<string>().Result;
+                        loenCategoria = JsonConvert.DeserializeObject<edClase>(rwsapilu);
+                    }
+                }
+
+                objResultado = new
+                {
+                    aaData = loenCategoria
+                };
+                return Json(objResultado);
+            }
+            catch (Exception ex)
+            {
+                //UtlLog.toWrite(UtlConstantes.PizarraWEB, UtlConstantes.LogNamespace_PizarraWEB, this.GetType().Name.ToString(), MethodBase.GetCurrentMethod().Name, UtlConstantes.LogTipoError, "", ex.StackTrace.ToString(), ex.Message.ToString());
+                return Json(ex);
+            }
+
+        }
+
+    }
+}
