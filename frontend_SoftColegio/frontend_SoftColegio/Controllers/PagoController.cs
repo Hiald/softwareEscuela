afccc892f7f49750692c83cc763470db4457b222
Hiald <jonathan.seminario11@gmail.com>
2021-01-13 18:03:13 -0500

    avance controlador pago

--- frontend_SoftColegio/frontend_SoftColegio/Controllers/PagoController.cs
+++ frontend_SoftColegio/frontend_SoftColegio/Controllers/PagoController.cs
@@ -9,12 +9,25 @@ using frontendED;
 using frontendUtil;
 using frontend_SoftColegio.Filters;
 using System.Text;
+using System.Web;
+using System.Linq;
 
 namespace frontend_SoftColegio.Controllers
 {
     public class PagoController : Controller
     {
-        // GET: Usuario
+        // alumno
+        [SecuritySession]
+        public ActionResult pago()
+        {
+            int irolusuario = UtlAuditoria.ObtenerTipoUsuario();
+            int idusuario = UtlAuditoria.ObtenerIdUsuario();
+            ViewBag.GrolUsuario = irolusuario;
+            ViewBag.Gidusuario = idusuario;
+            return View();
+        }
+
+        // admin
         [SecuritySession]
         public ActionResult pagoGestion()
         {
@@ -96,13 +109,6 @@ namespace frontend_SoftColegio.Controllers
             try
             {
                 var objResultado = new object();
-                int ItipoUsuario = UtlAuditoria.ObtenerTipoUsuario();
-                int IidUsuario = UtlAuditoria.ObtenerIdUsuario();
-                int idusuario = widusuario;
-                if (ItipoUsuario != 1)
-                {
-                    idusuario = IidUsuario;
-                }
                 List<edCurso> loenClase = new List<edCurso>();
                 using (var client = new HttpClient())
                 {
@@ -140,27 +146,206 @@ namespace frontend_SoftColegio.Controllers
 
         }
 
-        //ACTIVO : actualiza la clave del usuario seleccionado
+        //ACTIVO : registra los pagos tanto como admin y como usuario
         [HttpPost]
-        public async Task<JsonResult> RegistrarPago(int widusuario, int widnivel, int widgrado, int widcurso, string woperacion
-                , int wtipopago, int wtipomoneda, string wdescripcion, int wdia, int wmes, int wanio
-                , string whora, decimal wmonto, decimal wigv, string wimgr1, string wimgr2
+        public async Task<JsonResult> RegistrarPago(int widusuario, int widnivel, int widgrado
+                , int widcurso, string woperacion, int wtipopago, int wtipomoneda
+                , string wdescripcion, int wdia, int wmes, int wanio
+                , string whora, decimal wmonto, decimal wigv
+                , IEnumerable<HttpPostedFileBase> FRutaImagenes
                 , string wfinip, string wffinp, string wfr, string wff)
         {
             try
             {
                 var objResultado = new object();
-                string wfechaRegistro = DateTime.Now.ToString();
                 int idGenerado = -1;
                 Int16 estado = 1;
+
+                Random random = new Random();
+                const string alfabeto = "abcdefghijklmnopqrstuvwxyz0123456789";
+                var releaseUris = new List<string>();
+                foreach (var file in FRutaImagenes)
+                {
+                    if (file != null && file.ContentLength > 0)
+                    {
+                        string sTipoImagen = System.IO.Path.GetFileName(file.ContentType);
+                        string sRandom = new string(Enumerable.Repeat(alfabeto, 10).Select(s => s[random.Next(s.Length)]).ToArray());
+                        string sRutaLocal = System.IO.Path.Combine(Server.MapPath("~/Content/imgpago/"), sRandom + "." + sTipoImagen);
+
+                        string sRutaServidor = "/Content/imgpago/" + sRandom + "." + sTipoImagen;
+                        // sRuta es para la bd                        
+                        file.SaveAs(sRutaLocal);
+                        releaseUris.Add(sRutaServidor);
+                    }
+                }
+                string valorimg1 = "/Content/imgpago/vacio.png";
+                string valorimg2 = "/Content/imgpago/vacio.png";
+                if (releaseUris.Count == 1)
+                {
+                    valorimg1 = releaseUris[0];
+                    if (releaseUris.Count == 2)
+                    {
+                        valorimg2 = releaseUris[1];
+                    }
+                }
                 using (var client = new HttpClient())
                 {
                     client.BaseAddress = new Uri(MvcApplication.wsRouteSchoolBackend);
                     client.DefaultRequestHeaders.Clear();
                     client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
                     HttpResponseMessage ResRegistrarCuenta = await client.
-                            GetAsync("api/pago/wsRegistrarPago?wsidusuario=" + widusuario
-                        + "&wsnuevaclave=" + wnuevaclave);
+                    GetAsync("api/pago/wsRegistrarPago?widusuario=" + widusuario + "&widnivel=" + widnivel
+                    + "&widgrado=" + widgrado + "&widcurso=" + widcurso + "&woperacion=" + woperacion
+                    + "&wtipopago=" + wtipopago + "&wtipomoneda=" + wtipomoneda + "&wdescripcion=" + wdescripcion
+                    + "&wdia=" + wdia + "&wmes=" + wmes + "&wanio=" + wanio
+                    + "&whora=" + whora + "&wmonto=" + wmonto + "&wigv=" + wigv
+                    + "&wimgr1=" + valorimg1 + "&wimgr2=" + valorimg2
+                    + "&wfinip=" + wfinip + "&wffinp=" + wffinp + "&wfr=" + wfr + "&wff=" + wff);
+
+                    if (ResRegistrarCuenta.IsSuccessStatusCode)
+                    {
+                        var rwsapi = ResRegistrarCuenta.Content.ReadAsAsync<string>().Result;
+                        idGenerado = int.Parse(rwsapi);
+                        if (idGenerado == -1 || idGenerado == 0)
+                        {
+                            //error
+                            objResultado = new
+                            {
+                                iResultado = -1,
+                                iResultadoIns = "Ha ocurrido un error, intentalo nuevamente. Error: BCK"
+                            };
+                            return Json(objResultado);
+                        }
+                    }
+                }
+
+                objResultado = new
+                {
+                    iResultado = 1,
+                    iResultadoIns = "Registrado correctamente"
+                };
+                return Json(objResultado);
+            }
+            catch (Exception ex)
+            {
+                //UtlLog.toWrite(UtlConstantes.PizarraWEB, UtlConstantes.LogNamespace_PizarraWEB, this.GetType().Name.ToString(), MethodBase.GetCurrentMethod().Name, UtlConstantes.LogTipoError, "", ex.StackTrace.ToString(), ex.Message.ToString());
+                return Json(ex);
+            }
+
+        }
+
+        //ACTIVO : lista un valor para el modo usuario si faltan pocos tiempo al dia de pago
+        [HttpPost]
+        public async Task<JsonResult> NotificarPago(int widusuario, string wfechaacceso
+                                                , string wfechavalidar)
+        {
+            try
+            {
+                var objResultado = new object();
+                int idGenerado = -1;
+                using (var client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri(MvcApplication.wsRouteSchoolBackend);
+                    client.DefaultRequestHeaders.Clear();
+                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                    HttpResponseMessage ResRegistrarCuenta = await client.
+                            GetAsync("api/pago/wsNotificarPago?widusuario=" + widusuario
+                        + "&wfechaacceso=" + wfechaacceso + "&wfechavalidar=" + wfechavalidar);
+
+                    if (ResRegistrarCuenta.IsSuccessStatusCode)
+                    {
+                        var rwsapi = ResRegistrarCuenta.Content.ReadAsAsync<string>().Result;
+                        idGenerado = int.Parse(rwsapi);
+                        if (idGenerado == -1 || idGenerado == 0)
+                        {
+                            //error
+                            objResultado = new
+                            {
+                                iResultado = -1,
+                                iResultadoIns = "Ha ocurrido un error, intentalo nuevamente. Error: BCK"
+                            };
+                            return Json(objResultado);
+                        }
+                    }
+                }
+
+                objResultado = new
+                {
+                    iResultado = 1,
+                    iResultadoIns = "Registrado correctamente"
+                };
+                return Json(objResultado);
+            }
+            catch (Exception ex)
+            {
+                //UtlLog.toWrite(UtlConstantes.PizarraWEB, UtlConstantes.LogNamespace_PizarraWEB, this.GetType().Name.ToString(), MethodBase.GetCurrentMethod().Name, UtlConstantes.LogTipoError, "", ex.StackTrace.ToString(), ex.Message.ToString());
+                return Json(ex);
+            }
+
+        }
+
+        //ACTIVO : lista los pagos de los usuarios y admin
+        [HttpPost]
+        public async Task<JsonResult> RptListarSumPago(int widnivel, int widgrado, int widcurso
+                            , string wfechaini, string wfechafin, int wmes, int wanio)
+        {
+            try
+            {
+                var objResultado = new object();
+                List<edCurso> loenClase = new List<edCurso>();
+                using (var client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri(MvcApplication.wsRouteSchoolBackend);
+                    client.DefaultRequestHeaders.Clear();
+                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                    HttpResponseMessage Reslistarusu = await client.
+                        GetAsync("api/pago/wsRptListarSumaPagos?widnivel=" + widnivel
+                        + "&widgrado=" + widgrado + "&widcurso=" + widcurso
+                        + "&wfechaini=" + wfechaini + "&wfechafin=" + wfechafin
+                        + "&wmes=" + wmes + "&wanio=" + wanio);
+                    if (Reslistarusu.IsSuccessStatusCode)
+                    {
+                        var rwsapilu = Reslistarusu.Content.ReadAsAsync<string>().Result;
+                        loenClase = JsonConvert.DeserializeObject<List<edCurso>>(rwsapilu);
+                    }
+                }
+
+                objResultado = new
+                {
+                    PageStart = 1,
+                    pageSize = 100,
+                    SearchText = string.Empty,
+                    ShowChildren = UtlConstantes.bValorTrue,
+                    iTotalRecords = loenClase.Count,
+                    iTotalDisplayRecords = 1,
+                    aaData = loenClase
+                };
+                return Json(objResultado);
+            }
+            catch (Exception ex)
+            {
+                //UtlLog.toWrite(UtlConstantes.PizarraWEB, UtlConstantes.LogNamespace_PizarraWEB, this.GetType().Name.ToString(), MethodBase.GetCurrentMethod().Name, UtlConstantes.LogTipoError, "", ex.StackTrace.ToString(), ex.Message.ToString());
+                return Json(ex);
+            }
+
+        }
+
+        //ACTIVO : lista los usuarios totales para el modo admin
+        [HttpPost]
+        public async Task<JsonResult> ListarUsuarioTotal(int widnivel, int widgrado, int widcurso)
+        {
+            try
+            {
+                var objResultado = new object();
+                int idGenerado = -1;
+                using (var client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri(MvcApplication.wsRouteSchoolBackend);
+                    client.DefaultRequestHeaders.Clear();
+                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                    HttpResponseMessage ResRegistrarCuenta = await client.
+                            GetAsync("api/pago/wsListarUsuarioTotal?widnivel=" + widnivel
+                        + "&widgrado=" + widgrado + "&widcurso=" + widcurso);
 
                     if (ResRegistrarCuenta.IsSuccessStatusCode)
                     {
