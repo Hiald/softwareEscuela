42472a7852473f8ea600e7d344d3e7f7a98219e3
Hiald <jonathan.seminario11@gmail.com>
2021-02-09 14:14:20 -0500

    cambio en rutas web config y ccreacion de controlador subir video

--- frontend_SoftColegio/frontend_SoftColegio/Controllers/ClaseController.cs
+++ frontend_SoftColegio/frontend_SoftColegio/Controllers/ClaseController.cs
@@ -8,6 +8,8 @@ using System.Web.Mvc;
 using frontendED;
 using frontendUtil;
 using frontend_SoftColegio.Filters;
+using System.Linq;
+using System.Web;
 
 namespace frontend_SoftColegio.Controllers
 {
@@ -99,8 +101,8 @@ namespace frontend_SoftColegio.Controllers
         //ACTIVO : inserta la clase : admin
         [HttpPost]
         public async Task<JsonResult> InsertarClaseGestion(int idgrado, int isemana, string nombre, string descripcion
-            , string rutaenlace, string rutavideo, int categoria, string imagen, string imagenruta
-            , int orden)
+            , string rutaenlace, string rutavideo, int categoria, string imagenruta
+            , int orden, IEnumerable<HttpPostedFileBase> imagen)
         {
             try
             {
@@ -148,6 +150,94 @@ namespace frontend_SoftColegio.Controllers
             {
                 //UtlLog.toWrite(UtlConstantes.PizarraWEB, UtlConstantes.LogNamespace_PizarraWEB, this.GetType().Name.ToString(), MethodBase.GetCurrentMethod().Name, UtlConstantes.LogTipoError, "", ex.StackTrace.ToString(), ex.Message.ToString());
                 return Json(ex);
+            }
+
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> RegistrarClaseAdminDocente(int widgrado, int wisemana
+            , string wnombre, string wdescripcion, string wrutaenlace, string wrutavideo
+            , int wcategoria, string wimagenruta, int worden
+            , IEnumerable<HttpPostedFileBase> wimagen)
+        {
+            try
+            {
+                var objResultado = new object();
+                int idGenerado = -1;
+                Random random = new Random();
+                const string alfabeto = "abcdefghijklmnopqrstuvwxyz0123456789";
+                var releaseUris = new List<string>();
+                string wfechaRegistro = DateTime.Now.ToString();
+                Int16 estado = 1;
+                foreach (var file in wimagen)
+                {
+                    if (file != null && file.ContentLength > 0)
+                    {
+                        string sTipoImagen = System.IO.Path.GetFileName(file.ContentType);
+                        string sRandom = new string(Enumerable.Repeat(alfabeto, 10).Select(s => s[random.Next(s.Length)]).ToArray());
+                        string sRutaLocal = System.IO.Path.Combine(Server.MapPath("~/clasevideo/"), sRandom + "." + sTipoImagen);
+
+                        string sRutaServidor = "/clasevideo/" + sRandom + "." + sTipoImagen;
+                        // sRuta es para la bd                        
+                        file.SaveAs(sRutaLocal);
+                        releaseUris.Add(sRutaServidor);
+                    }
+                }
+                string valorimg1 = "/Content/imgpago/vacio.png";
+                string valorimg2 = "/Content/imgpago/vacio.png";
+                if (releaseUris.Count == 1)
+                {
+                    valorimg1 = releaseUris[0];
+                }
+                if (releaseUris.Count == 2)
+                {
+                    valorimg1 = releaseUris[0];
+                    valorimg2 = releaseUris[1];
+                }
+                
+                    //si el pago es individual
+                    using (var client = new HttpClient())
+                    {
+                        client.BaseAddress = new Uri(MvcApplication.wsRouteSchoolBackend);
+                        client.DefaultRequestHeaders.Clear();
+                        client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                            HttpResponseMessage ResRegistrarCuenta = await 
+                            client.GetAsync("api/clase/wsInsertarClase?wsidcurso=" + widgrado
+                         + "&wssemana=" + wisemana + "&wsnombre=" + wnombre + "&wsdescripcion=" 
+                         + wdescripcion + "&wsrutaenlace=" + wrutaenlace + "&wsrutavideo=" 
+                         + wrutavideo + "&wscategoria=" + wcategoria + "&wsimagen=" + wimagen 
+                         + "&wsimagenruta=" + wimagenruta + "&wsorden=" + worden
+                         + "&wsestado=" + estado + "&wfecharegistro=" + wfechaRegistro);
+
+                    if (ResRegistrarCuenta.IsSuccessStatusCode)
+                        {
+                            var rwsapi = ResRegistrarCuenta.Content.ReadAsAsync<string>().Result;
+                            idGenerado = int.Parse(rwsapi);
+                            if (idGenerado == -1)
+                            {
+                                //error
+                                objResultado = new
+                                {
+                                    iResultado = -1,
+                                    iResultadoIns = "Ha ocurrido un error, intentalo nuevamente. Error: BCK"
+                                };
+                                return Json(objResultado);
+                            }
+                        }
+                    }
+                
+               
+                objResultado = new
+                {
+                    iResultado = 1,
+                    iResultadoIns = "Registrado correctamente"
+                };
+                return RedirectToAction("pagoGestion", "pago");
+            }
+            catch (Exception ex)
+            {
+                //UtlLog.toWrite(UtlConstantes.PizarraWEB, UtlConstantes.LogNamespace_PizarraWEB, this.GetType().Name.ToString(), MethodBase.GetCurrentMethod().Name, UtlConstantes.LogTipoError, "", ex.StackTrace.ToString(), ex.Message.ToString());
+                return RedirectToAction("index", "Login", new { ivalorsesion = 1, valorlogin = ex.Message });
             }
 
         }
